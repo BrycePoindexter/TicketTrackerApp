1d94abc9e6371d4cde41c1e99836d4e9fd728956
Bryce Poindexter <pogibryce1@gmail.com>
2022-08-06 00:11:03 -0400

    Add tracker class

---
+++ TicketTrackerLibrary/Tracker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketTrackerLibrary
+{
+    public class Tracker
+    {
+        public List<Ticket> TicketList { get; set; }
+        public List<User> UserList { get; set; }
+
+        public Tracker()
+        {
+            TicketList = new List<Ticket>();
+            UserList = new List<User>();
+        }
+    }
+}
